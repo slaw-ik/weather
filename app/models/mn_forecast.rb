3947c8ad7756571aaf999d73fd4d1375a81e44b7
miroslav <slaw-ik@yandex.ru>
2013-04-28 16:40:04 +0300

    implemment model logic

--- app/models/mn_forecast.rb
+++ app/models/mn_forecast.rb
@@ -1,42 +1,45 @@
 class MnForecast < ActiveRecord::Base
   attr_accessible :daytime_high_temp, :description, :f_date, :morning_low_temp, :prec_day, :prec_night, :weather_id
+  belongs_to :state, :foreign_key => :state_id, :class_name => "MnState"
 
   def self.get_forecast(state_id)
     @forecasts = self.where(:state_id => state_id)
     unless @forecasts.empty?
-      if @forecasts.first.try(:updated_at) < DteTime.now - 3.hours
-        @forecasts.destroy_all
+      if @forecasts.first.try(:updated_at).try(:utc) < DateTime.now.utc - 3.hours
+        destroy_all(:state_id => state_id)
         @forecasts = fetch_forecasts(state_id)
       end
     else
       @forecasts = fetch_forecasts(state_id)
     end
-
-    #client = Savon.client(wsdl: "http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL")
-    #response = client.call(:get_city_forecast_by_zip, message: { "ZIP" => "90001" })
-    #forecast = response.hash[:envelope][:body][:get_city_forecast_by_zip_response][:get_city_forecast_by_zip_result][:forecast_result][:forecast]
-
-
-    #{ :date=>Thu, 18 Apr 2013 00:00:00 +0000,
-    #  :weather_id=>"4",
-    #  :desciption=>"Sunny",
-    #  :temperatures=>{
-    #    :morning_low=>"53",
-    #    :daytime_high=>"83"
-    #  },
-    #  :probability_of_precipiation=>{
-    #    :nighttime=>"00",
-    #    :daytime=>"00"
-    #  }
-    #}
-
-
+    return @forecasts
   end
 
   private
 
   def self.fetch_forecasts(state_id)
-     puts ("Fetching..." + state_id.to_s)
+
+    state = MnState.find_by_id(state_id)
+
+    client = Savon.client(wsdl: "http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL")
+    response = client.call(:get_city_forecast_by_zip, message: {"ZIP" => "#{state.zip}"})
+    forecasts = response.hash[:envelope][:body][:get_city_forecast_by_zip_response][:get_city_forecast_by_zip_result][:forecast_result][:forecast]
+
+    result = []
+    forecasts.each do |forecast|
+      result << MnForecast.new(
+          :f_date => forecast[:date],
+          :description => forecast[:desciption],
+          :morning_low_temp => forecast[:temperatures][:morning_low],
+          :daytime_high_temp => forecast[:temperatures][:daytime_high],
+          :prec_day => forecast[:probability_of_precipiation][:daytime],
+          :prec_night => forecast[:probability_of_precipiation][:nighttime],
+          :weather_id => forecast[:weather_id]
+      )
+    end
+
+    state.forecasts << result
+    return result
   end
 
 end
--- app/models/mn_state.rb
+++ app/models/mn_state.rb
@@ -1,3 +1,4 @@
 class MnState < ActiveRecord::Base  
   attr_accessible :name, :zip
+  has_many :forecasts, :foreign_key => :state_id, :class_name => "MnForecast"
 end
